6c70951e3526e00e4c1268e613220ad09f914739
David Garcia Varas <davidga2@c1r13s5.42madrid.com>
2023-05-27 07:16:11 +0200

    Hasta RAM

---
+++ monitoring.sh
@@ -0,0 +1,49 @@
+# --- VARIABLES ---
+
+# ARCHITECTURE and KERNEL VERSION
+arch=$(uname -m)
+kernel=$(uname -r)
+
+# NUMBER OF PHYSICAL CORES
+cores=$(nproc)
+
+# NUMBER OF VIRTUAL CORES
+v_cores=$(nproc --all --ignore=1)
+
+# CURRENTLY AVAILABLE RAM ON THE SERVER AND PERCENTAGE OF USE
+ca_ram=$(free -m | grep "Mem:" | awk '{print $4}')
+total_ram=$(free -m | grep "Mem:" | awk '{print $2}')
+piu_ram=$(free -m | grep "Mem:" | awk '{print $3*100/$2}')
+
+# MEMORY AVAILABLE ON THE SERVER AND PERCENTAJE OF USE
+
+# CURRENT PERCENTAJE OF USE OF THE NUCLEOS
+
+# DATE AND TIME OF THE LAST REBOOT
+
+# LVM IS ACTIVE OR NOT
+
+# NUMBER OF ACTIVES CONNECTIONS (TCP)
+
+# NUMBER OF USERS LOGGED IN TO THE SERVER
+
+# SERVER IPv4 ADRESS AND MAC
+
+# NUMBER OF COMMANDS EXECUTED WITH SUDO
+
+# --- WRITE ALL COMMAND ---
+wall "	¡Hola, amigos del Youtube! (...)
+
+	I mean... HELLO, AMIGOS DEL DEBIAN!!!
+
+	This is a message from your favorite superuser 'r41z-r00t'
+	with some funzy info about the server. I hope you enjoy it.
+	If not... I don't care! You will see it again in 10 minutes.
+
+		- Architecture:				$arch
+		- Kernel:					$kernel
+		- Phisical cores:			$cores
+		- Virtual cores:			$v_cores
+		- Currently available RAM:	$ca_ram/$total_ram
+		- RAM percentaje in use:	$piu_ram%
+		"
