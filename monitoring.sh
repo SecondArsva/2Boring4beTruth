577485d8895e98525abf0d43440c73d69208cd50
David Garcia Varas <davidga2@c1r4s2.42madrid.com>
2023-05-31 10:41:21 +0200

    FALTA SUDO

--- monitoring.sh
+++ monitoring.sh
@@ -37,8 +37,8 @@ tcp=$(grep 'TCP' /proc/net/sockstat | awk '{print($3)}')
 users=$(users | wc -w)
 
 # SERVER IPv4 ADRESS AND MAC
-ipv4=$(hostname -I | awk '{print $1}')
-mac=$()
+ipv4=$(ip -4 address show enp0s3 | grep "inet" | awk '{print $2}' | cut -d "/" -f 1)
+mac=$(ip a | grep "link/ether" | awk '{print $2}')
 
 # NUMBER OF COMMANDS EXECUTED WITH SUDO
 sudo_log=$()
@@ -52,20 +52,20 @@ wall "	¡Hola, amigos del Youtube! (...)
 	with some funzy info about the server. I hope you enjoy it.
 	If not... I don't care! You will see it again in 10 minutes.
 
-		- Architecture:				$arch
-		- Kernel:					$kernel
-		- Physical cores:			$cores
-		- Virtual cores:i			$v_cores
-		- Currently available RAM:	$ca_ram / $total_ram
-		- RAM percentaje in use:	$piu_ram%
-		- Available disk storage:	$ca_disk / $total_disk
-		- Disk storage used:		$piu_disk
-		- Current cores use:		$core_use%
-		- Last reboot:				$last_reboot_date $last_reboot_hour
-		- LMV use:					$lvm_use
-		- Connexions TCP:			$tcp
-		- User log:					$users
-		- IPv4:					$ipv4
-		- MAC:					$mac
-		- Sudo:						$sudo_log
+		- Architecture:					$arch
+		- Kernel:						$kernel
+		- Physical cores:				$cores
+		- Virtual cores:i				$v_cores
+		- Currently available RAM:		$ca_ram / $total_ram
+		- RAM percentaje in use:		$piu_ram%
+		- Available disk storage:		$ca_disk / $total_disk
+		- Disk storage used:			$piu_disk
+		- Current cores use:			$core_use%
+		- Last reboot:					$last_reboot_date $last_reboot_hour
+		- LMV use:						$lvm_use
+		- Connexions TCP:				$tcp
+		- User log:						$users
+		- IPv4:							$ipv4
+		- MAC:							$mac
+		- Commands executed with sudo:	$sudo_log
 		"
